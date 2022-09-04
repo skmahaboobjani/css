46147926bc1dd6656d45c874d42d666c97149fd1
mahaboob jani <skmabujani521@gmail.com>
2022-09-04 17:46:14 +0530

    mabu

---
+++ src/components/Inline.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const heading = {
+    color:"blue",
+    fontSize:"80px",
+   
+
+}
+
+function inline()
+{
+    return (
+        <div>
+            <h1 style={heading}>inline</h1>
+        </div>
+    )
+}
+
+export default   inline;
---
+++ src/components/Stylesheet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './myStyle.css';
+function Stylesheet()
+{
+    return (
+        <div>
+            <h1 className='primary'>stylesheets</h1>
+        </div>
+    )
+}
+export default Stylesheet
